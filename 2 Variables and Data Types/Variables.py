8b8c72714d788787798385bf4c624e2b7247952f
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-09-27 12:09:45 +0530

    Add files via upload

---
+++ 2 Variables and Data Types/Variables.py
@@ -0,0 +1,18 @@
+# Defining variables
+Name = "Peeyush" 
+Age = 19
+Height_cm = 185.42
+Repository_helpful = True
+Null = None
+# Printing the variables
+print(Name)
+print(Age)
+print(Height_cm)
+print(Repository_helpful)
+print(Null)
+# Printing the type of varaibles
+print(type(Name))
+print(type(Age))
+print(type(Height_cm))
+print(type(Repository_helpful))
+print(type(Null))
