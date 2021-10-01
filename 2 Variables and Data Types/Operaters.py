b32c7a56697f78ff98ac1df158bb0067f4d06f66
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-01 23:08:29 +0530

    Update Operaters.py

--- 2 Variables and Data Types/Operaters.py
+++ 2 Variables and Data Types/Operaters.py
@@ -1,1 +1,25 @@
+a = 3
+b = 4
 
+# Arithmentic Operators
+print("The value of 3+4 is:", 3+4)
+print("The value of 3-4 is:", 3-4)
+print("The value of 3*4 is:", 3*4)
+print("The value of 3/4 is:", 3/4)
+
+# Assignment Operators
+a = 38
+a += 2
+print("The value of a is", a)
+
+# Comparison Operators // it returns a boolean value
+v = (5!=3)
+print(v)
+w = (5==3)
+print(w)
+x = (5>3)
+print(x)
+y = (5<3)
+print(y)
+z = (5>=5)
+print(z)
