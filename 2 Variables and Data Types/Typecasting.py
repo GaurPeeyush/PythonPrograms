197895a608e4b2367b56c7cd287a6ddca64b556b
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-10-02 23:53:28 +0530

    Update Typecasting.py

--- 2 Variables and Data Types/Typecasting.py
+++ 2 Variables and Data Types/Typecasting.py
@@ -1,1 +1,9 @@
-
+a = "50" #string literal
+print("Type of a is:", type(a))
+print(a)
+a = int(a)
+print("Type of a here is:", type(a))
+print(a+10)
+a = float(a)
+print("Type of a here is:", type(a))
+print(a+10)
