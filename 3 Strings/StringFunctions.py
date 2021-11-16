f835e7cbc95c6ec738f7f5e2298339f5fc49a124
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-11-16 20:35:45 +0530

    Add files via upload

---
+++ 3 Strings/StringFunctions.py
@@ -0,0 +1,7 @@
+s = "hello, My name is Peeyush"
+print(len(s))
+print(s.count("is"))
+print(s.find("Peeyush"))
+print(s.replace("Peeyush", "Peeyush Gaur"))
+print(s.capitalize())
+print(s.endswith("Peeyush"))
