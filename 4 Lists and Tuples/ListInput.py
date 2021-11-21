80641cb0681c41795808457e0c41431bc968305b
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-11-21 20:39:20 +0530

    Add files via upload

---
+++ 4 Lists and Tuples/ListInput.py
@@ -0,0 +1,6 @@
+li = []
+n = int(input("Enter the number of elements to add: \n"))
+for i in range(n):
+    print("Enter the name of fruit: ")
+    li.append(input())
+print(f"The list of fruits is: {li}")
