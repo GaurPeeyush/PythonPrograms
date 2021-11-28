e3ac542f6afb93c8c7650468c271f9ea3ccda42d
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-11-28 13:14:39 +0530

    Add files via upload

---
+++ 6 Conditional Expressions/SpamComment.py
@@ -0,0 +1,11 @@
+n = input("Enter the comment\n")
+if "buy now" in n:
+    print("SPAM")
+elif "subscribe this" in n:
+    print("SPAM")
+elif "make a lot of money" in n:
+    print("SPAM")
+elif "click this" in n:
+    print("SPAM")
+else:
+    print("Not a SPAM comment")
