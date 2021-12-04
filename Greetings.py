901b661099b6199c06e653db96b9fc268dd1be27
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-12-04 20:18:05 +0530

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,5 @@
+# program to greet people whose name starts with S
+li = ["Peeyush", "Sachin", "Sakshi", "Sohan", "Mohit", "Samiksha", "Nishica"]
+for name in li:
+    if name.startswith("S"):
+        print("Hey" + " " + str(name))
