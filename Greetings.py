066eae18c825ae0c24dec2a3e1179e6588a2a8ab
Peeyush Gaur <81735768+GaurPeeyush@users.noreply.github.com>
2021-12-04 20:19:30 +0530

    Delete Greetings.py

--- Greetings.py
+++
@@ -1,5 +0,0 @@
-# program to greet people whose name starts with S
-li = ["Peeyush", "Sachin", "Sakshi", "Sohan", "Mohit", "Samiksha", "Nishica"]
-for name in li:
-    if name.startswith("S"):
-        print("Hey" + " " + str(name))
